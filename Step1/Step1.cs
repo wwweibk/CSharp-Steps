cfe53f629c59f50efcf23c659ead43b628ea1e67
Viktoria Petrova <valeradurov.luvsd@gmail.com>
2024-06-17 20:08:18 +0300

    Update Step1.cs

--- Step1/Step1.cs
+++ Step1/Step1.cs
@@ -1,75 +1,52 @@
-using Kompas6API5;
-
 using System;
-using System.IO;
 using Microsoft.Win32;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Globalization;
 using KAPITypes;
+using System.Resources;
+using Kompas6API5;
 
-namespace Steps.NET
-{
-	// Класс Step1 - Самая простая библиотека на C#
-  [ClassInterface(ClassInterfaceType.AutoDual)]
- 	public class Step1
-	{
-		// Имя библиотеки
-		[return: MarshalAs(UnmanagedType.BStr)] public string GetLibraryName()
-		{
-			return "Step1 - Самая простая библиотека на C#";
-		}
-		
+namespace Step1 {
+    // --------------------------------------------------------------------------------
+    // РџРµСЂРµСЃРµС‡РµРЅРёРµ РґРІСѓС… РєСЂРёРІС‹С….
+    // Р’РѕР·РІСЂР°С‰Р°РµС‚:
+    // 1 СѓСЃРїРµС€РЅРѕРµ Р·Р°РІРµСЂС€РµРЅРёРµ
+    // 0 РєСЂРёРІС‹Рµ РЅРµ РїРµСЂРµСЃРµРєР°СЋС‚СЃСЏ РёР»Рё СЃРѕРІРїР°РґР°СЋС‚
+    // -1 РїРµСЂРІС‹Р№ РѕР±СЉРµРєС‚ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+    // -2 РІС‚РѕСЂРѕР№ РѕР±СЉРµРєС‚ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+    // -3 РєСЂРёРІС‹Рµ СЂР°СЃРїРѕР»РѕР¶РµРЅС‹ РІ СЂР°Р·РЅС‹С… РІРёРґР°С…
+    // -4 РЅРµ СЃРѕРІРїР°РґР°СЋС‚ РЎРљ РѕРїСЂРµРґРµР»РµРЅРёСЏ РєСЂРёРІС‹С… 
+    // -5 РїРµСЂРІС‹Р№ РѕР±СЉРµРєС‚ РЅРµ СЏРІР»СЏРµС‚СЃСЏ РєСЂРёРІРѕР№
+    // -6 РІС‚РѕСЂРѕР№ РѕР±СЉРµРєС‚ РЅРµ СЏРІР»СЏРµС‚СЃСЏ РєСЂРёРІРѕР№
+    // -7 РѕС€РёР±РєР°
+    // РґРёРЅР°РјРёС‡РµСЃРєРёР№ РјР°СЃСЃРёРІ С‚РѕС‡РµРє РїРµСЂРµСЃРµС‡РµРЅРёСЏ array РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ СЃРѕР·РґР°РЅ РґРѕ РїРµСЂРµРґР°С‡Рё РµРіРѕ РІ С„СѓРЅРєС†РёСЋ.
+    // ---------------------------------------------------------------------------------
 
-		// Головная функция библиотеки
-		public void ExternalRunCommand([In] short command, [In] short mode, [In, MarshalAs(UnmanagedType.IDispatch)] object kompas_)
-		{
-			KompasObject kompas = (KompasObject) kompas_;
-			kompas.ksMessage("Привет!");
-		}
+    [DllImport("KompasLibrary.dll", CallingConvention = CallingConvention.StdCall)]
+    public static extern int ksIntersectCurvCurv(IntPtr p1, IntPtr p2, IntPtr pArr);
+    // --------------------------------------------------------------------------------
+    // РџРµСЂРµСЃРµС‡РµРЅРёРµ РґРІСѓС… РєСЂРёРІС‹С….
+    // ---------------------------------------------------------------------------------
+    [DllImport("KompasLibrary.dll", CallingConvention = CallingConvention.StdCall)]
+    public static extern void IntersectCurvCurvEx(IntPtr p1, IntPtr p2, ref int kp, IntPtr xp, IntPtr yp, int maxCount, int touchInclude);
 
-    // Номер ключа защиты  short  или int16
-    public short GetProtectNumber()
-    {
-      return 111;
-    }
 
-    #region COM Registration
-    // Эта функция выполняется при регистрации класса для COM
-    // Она добавляет в ветку реестра компонента раздел Kompas_Library,
-    // который сигнализирует о том, что класс является приложением Компас,
-    // а также заменяет имя InprocServer32 на полное, с указанием пути.
-    // Все это делается для того, чтобы иметь возможность подключить
-    // библиотеку на вкладке ActiveX.
-    [ComRegisterFunction]
-		public static void RegisterKompasLib(Type t)
-		{
-			try
-			{
-				RegistryKey regKey = Registry.LocalMachine;
-				string keyName = @"SOFTWARE\Classes\CLSID\{" + t.GUID.ToString() + "}";
-				regKey = regKey.OpenSubKey(keyName, true);
-				regKey.CreateSubKey("Kompas_Library");
-				regKey = regKey.OpenSubKey("InprocServer32", true);
-				regKey.SetValue(null, System.Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\mscoree.dll");
-				regKey.Close();
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(string.Format("При регистрации класса для COM-Interop произошла ошибка:\n{0}", ex));
-			}
-		}
-		
-		// Эта функция удаляет раздел Kompas_Library из реестра
-		[ComUnregisterFunction]
-		public static void UnregisterKompasLib(Type t)
-		{
-			RegistryKey regKey = Registry.LocalMachine;
-			string keyName = @"SOFTWARE\Classes\CLSID\{" + t.GUID.ToString() + "}";
-			RegistryKey subKey = regKey.OpenSubKey(keyName, true);
-			subKey.DeleteSubKey("Kompas_Library");
-			subKey.Close();
-		}
-		#endregion
-	}
+    // --------------------------------------------------------------------------------
+    // РџРµСЂРµСЃРµС‡РµРЅРёРµ РґРІСѓС… РєСЂРёРІС‹С….
+    // Р’РѕР·РІСЂР°С‰Р°РµС‚:
+    // 1 СѓСЃРїРµС€РЅРѕРµ Р·Р°РІРµСЂС€РµРЅРёРµ
+    // 0 РєСЂРёРІС‹Рµ РЅРµ РїРµСЂРµСЃРµРєР°СЋС‚СЃСЏ РёР»Рё СЃРѕРІРїР°РґР°СЋС‚
+    // -1 РїРµСЂРІС‹Р№ РѕР±СЉРµРєС‚ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+    // -2 РІС‚РѕСЂРѕР№ РѕР±СЉРµРєС‚ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚
+    // -3 РєСЂРёРІС‹Рµ СЂР°СЃРїРѕР»РѕР¶РµРЅС‹ РІ СЂР°Р·РЅС‹С… РІРёРґР°С…
+    // -4 РЅРµ СЃРѕРІРїР°РґР°СЋС‚ РЎРљ РѕРїСЂРµРґРµР»РµРЅРёСЏ РєСЂРёРІС‹С… (РіРµРѕРј Рё Р°РЅРЅ) (?)
+    // -5 РїРµСЂРІС‹Р№ РѕР±СЉРµРєС‚ РЅРµ СЏРІР»СЏРµС‚СЃСЏ РєСЂРёРІРѕР№
+    // -6 РІС‚РѕСЂРѕР№ РѕР±СЉРµРєС‚ РЅРµ СЏРІР»СЏРµС‚СЃСЏ РєСЂРёРІРѕР№
+    // -7 РѕС€РёР±РєР°
+    // ---------------------------------------------------------------------------------
+    [DllImport("KompasLibrary.dll", CallingConvention = CallingConvention.StdCall)]
+    public static extern int ksIntersectCurvCurvEx(IntPtr p1, IntPtr p2, IntPtr pArr, int touchInclude);
+ 
 }
